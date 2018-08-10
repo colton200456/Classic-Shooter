631f167dddd05305f81adb2a51e2cffbcf49363d
Unknown <pierluc0414@live.ca>
2018-08-10 18:10:48 -0400

    first movement
    player can now move but not rotate + migrated to the latest unity project

---
+++ Classic Shooter/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject target;
+    public float angle;
+    public float height;
+
+    	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void LateUpdate () {
+        //transform.position = target.transform.position;
+        //transform.position -= Vector3.forward * angle;
+        //transform.position = new Vector3(transform.position.x, height, transform.position.z);
+        //transform.LookAt(target.transform.position);
+	}
+}
---
+++ Classic Shooter/Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+
+
+    protected Rigidbody MyRb;
+
+    // Use this for initialization
+    void Start()
+    {
+        MyRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.Translate(x * Time.deltaTime * speed, y * Time.deltaTime * speed, 0.0f, Space.World);
+
+        #warning THIS ISN'T WORKING, NEED TO DEBUG WHY?  http://wiki.unity3d.com/index.php/Xbox360Controller
+        float rx = Input.GetAxis("RightStickHorizontal");
+        float ry = Input.GetAxis("RightStickVertical");
+
+        float angle = Mathf.Atan2(rx, ry);
+
+        transform.rotation = Quaternion.Euler(0, angle, 0);
+    }
+}
