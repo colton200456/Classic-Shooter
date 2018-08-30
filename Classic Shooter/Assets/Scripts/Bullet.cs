12c87ff55e8d860fcd6a1aebd1c5fc1f146ef83e
Unknown <pierluc0414@live.ca>
2018-08-29 20:42:57 -0400

    fixed(?? or almost) rotation.

--- Classic Shooter/Assets/Scripts/Bullet.cs
+++ Classic Shooter/Assets/Scripts/Bullet.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour {
-
+public class Bullet : MonoBehaviour
+{
     public float shotRotation;
 
-	void Start () {
+    void Start()
+    {
         float rightStickx = Input.GetAxis("Right_Horizontal");
         float rightSticky = Input.GetAxis("Right_Vertical");
 
@@ -32,5 +33,13 @@ public class Bullet : MonoBehaviour {
         }
         GetComponent<Rigidbody2D>().velocity = new Vector3(-rightStickx, rightSticky, 0);
         GetComponent<Transform>().eulerAngles = new Vector3(0, 0, shotRotation);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "BulletDestroyer")
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
